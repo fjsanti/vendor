5dd0f2b1fc31931b737d60e54cf96f06cd76fc62
fjsanti <black02a98@gmail.com>
2019-06-24 13:30:17 -0500

    remove android.hardware.drm

--- audio.sh
+++
@@ -1,8 +0,0 @@
-rm ./system/lib/android.hardware.audio@4.0.so
-rm ./system/lib/android.hardware.audio@2.0.so
-rm ./system/lib/android.hardware.audio.effect@4.0.so
-rm ./system/lib/android.hardware.audio.effect@2.0.so
-rm ./system/lib64/android.hardware.audio@4.0.so
-rm ./system/lib64/android.hardware.audio@2.0.so
-rm ./system/lib64/android.hardware.audio.effect@4.0.so
-rm ./system/lib64/android.hardware.audio.effect@2.0.so
