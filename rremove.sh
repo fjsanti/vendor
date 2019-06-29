60ea3a5ff398d38e4be5ab31d1dca7b42d9e56df
fjsanti <black02a98@gmail.com>
2019-06-29 01:33:26 -0500

    remove audio files pushed at dt

---
+++ rremove.sh
@@ -0,0 +1,5 @@
+rm vendor/etc/audio_effects.xml
+rm vendor/etc/audio_ext_spkr.conf
+rm vendor/etc/audio_platform_info.xml
+rm vendor/etc/audio_policy_configuration.xml
+rm vendor/etc/mixer_paths.xml
