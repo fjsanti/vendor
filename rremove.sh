aea119e1d761c1dd15f2c75dd9a48cabf5462d59
fjsanti <black02a98@gmail.com>
2019-06-29 01:41:43 -0500

    remove more autogenerated files

--- rremove.sh
+++ rremove.sh
@@ -1,5 +1,3 @@
-rm vendor/etc/audio_effects.xml
-rm vendor/etc/audio_ext_spkr.conf
-rm vendor/etc/audio_platform_info.xml
-rm vendor/etc/audio_policy_configuration.xml
-rm vendor/etc/mixer_paths.xml
+rm vendor/etc/media_codecs_google_audio.xml
+rm vendor/etc/media_codecs_google_telephony.xml
+rm vendor/etc/media_codecs_google_video.xml
